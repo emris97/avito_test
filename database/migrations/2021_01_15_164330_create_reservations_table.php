e98932915ab9e98427ef2c90ded89f869e622f27
npoletaev <npoletaev97@gmail.com>
2021-01-17 13:25:58 +0400

    add migrations

---
+++ database/migrations/2021_01_15_164330_create_reservations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id('booking_id');
+            $table->unsignedBigInteger('room_id');
+            $table->foreign('room_id')->references('room_id')->on('rooms')->onDelete('cascade');
+            $table->date('date_start')->nullable();
+            $table->date('date_end')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+}
